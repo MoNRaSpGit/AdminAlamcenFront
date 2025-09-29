a2ffe23fb152aa78fdcebe561bedc7a40de1c3bd
MoNRaSpGit <ju4nrsuarez@gmail.com>
2025-09-28 23:34:57 -0300

    tu vieja

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Si está en producción (GitHub Pages), usa la carpeta del repo
-// Si está en desarrollo (local), base es "/"
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/AdminAlamcenFront/" : "/",
+  base: "/AdminAlamcenFront/", // 👈 nombre EXACTO del repo
 });
